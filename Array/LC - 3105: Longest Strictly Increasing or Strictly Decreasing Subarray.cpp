a8310cacad400c0ba03b78ae26827b386396cc27
TheRishavRoy <the.rishav.roy@gmail.com>
2025-02-03 21:58:45 +0530

    LC - 3105: Longest Strictly Increasing or Strictly Decreasing Subarray.cpp

---
+++ Array/LC - 3105: Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -0,0 +1,45 @@
+/*
+The idea here is to start from array idx=0  and initialize maxLen=1
+Now we parse fron idx=1 to idx=n-1 using recursion with ahelper function, and keep updating the incLen and decLen, while keeping track of maxLen found so far.
+We reset the counters whenever strictly inc or dec is violated.
+
+T.C.: O(N) . N= size of array
+
+The recursive function processes each element exactly once and moves to the next index in a straightforward manner. 
+Since there are no overlapping subproblems or branching recursions (like divide-and-conquer), the time complexity is:
+
+*/
+class Solution {
+public:
+    int longestSubarrayHelper(vector<int>& nums, int idx, int incLen, int decLen, int& maxLen)
+    {
+        if (idx == nums.size()) 
+            return maxLen; // Base case: end of array
+        
+        if(nums[idx]>nums[idx-1])
+        {
+            incLen++;
+            decLen=1;//reset
+        }
+        else if(nums[idx]<nums[idx-1])
+        {
+            decLen++;
+            incLen=1;//reset
+        }
+        else//cound be nums[i]==nums[i-1]. which neither strictly inc or dec
+        {
+            decLen=1;//reset
+            incLen=1;//reset
+        }
+        maxLen = max(maxLen, max(incLen, decLen)); // Update max length
+        return longestSubarrayHelper(nums, idx + 1, incLen, decLen, maxLen);//keep increasing the index till we hit base condition
+    }
+    int longestMonotonicSubarray(vector<int>& nums) 
+    {
+        if(nums.empty())
+            return 0;
+        
+        int maxLen = 1;
+        return longestSubarrayHelper(nums, 1, 1, 1, maxLen);//array,idx,incLen,decLen,maxLen
+    }
+};
