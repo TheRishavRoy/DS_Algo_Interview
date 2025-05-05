a6da73fe1160a44624e47dda39ecd7f352e39cfb
TheRishavRoy <the.rishav.roy@gmail.com>
2025-05-05 12:37:35 +0530

    LC - 1800 : Maximum Ascending Subarray Sum.cpp

--- Array/LC - 1800 : Maximum Ascending Subarray Sum.cpp
+++ Array/LC - 1800 : Maximum Ascending Subarray Sum.cpp
@@ -1,4 +1,5 @@
 /*
+Approach 1: Recursive
 same approach as in LC 3105
 T.C. : O(N) N is number of eleemnts in vector
 */
@@ -25,5 +26,37 @@ public:
         int ascSum=nums[0];//initialize our sum with value of first element in array. we start our subarray from here
         int maxSum=nums[0];//we keep updating maxSum
         return helperSum(1, nums, ascSum, maxSum);//we keep parsing nums starting from nums[1]
+    }
+};
+
+/*===========================================*
+Approach 2: Kadane's algorithm (Iterative):
+Use curSum and maxSum
+*============================================*/
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) 
+    {
+        if (nums.empty()) 
+        {
+            return 0;
+        }
+        
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        
+        for (int i = 1; i < nums.size(); ++i) 
+        {
+            if (nums[i] > nums[i - 1]) 
+            {
+                currentSum += nums[i];
+            } 
+            else 
+            {
+                currentSum = nums[i];
+            }
+            maxSum = max(maxSum, currentSum);
+        }
+        return maxSum;
     }
 };
