d7e10813eb459730b87c24463dd3f96661cb6ee4
TheRishavRoy <the.rishav.roy@gmail.com>
2025-05-11 16:24:38 +0530

    Create (Greedy) LC - 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp

---
+++ Array/(Greedy) LC - 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2)
+    {
+        long long sum1 = 0;
+        long long sum2 = 0;
+        int numZero1 = 0;
+        int numZero2 = 0;
+
+        for (int num : nums1) {
+            sum1 += num;
+            if (num == 0) {
+                ++numZero1;
+            }
+        }
+
+        for (int num : nums2) {
+            sum2 += num;
+            if (num == 0) {
+                ++numZero2;
+            }
+        }
+
+        long long minSum1 = sum1 + numZero1;//this much we have to add to first sum to get rid off all zeros
+        //this is because:
+        //we have to replace numZero1 number of Zeros with atleast value of 1
+        long long minSum2 = sum2 + numZero2;
+        //same goes for min sum that will be required for second sum
+
+        if (numZero1 == 0 && sum1 < minSum2) {
+            return -1;
+        }
+        if (numZero2 == 0 && sum2 < minSum1) {
+            return -1;
+        }
+
+        return max(minSum1, minSum2);
+    }
+};
