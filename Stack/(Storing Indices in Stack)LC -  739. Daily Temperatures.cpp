7a26a09fb2cea5497d1e43159a75851400bc5798
TheRishavRoy <the.rishav.roy@gmail.com>
2025-05-14 16:24:50 +0530

    Update (Storing Indices in Stack)LC -  739. Daily Temperatures.cpp

--- Stack/(Storing Indices in Stack)LC -  739. Daily Temperatures.cpp
+++ Stack/(Storing Indices in Stack)LC -  739. Daily Temperatures.cpp
@@ -8,8 +8,9 @@ public:
         {
             while (!stk.empty() && temperatures[stk.top()] < temperatures[i])//if satck is non empty && tempearture on top of stack is less than current temperature. update the same for the top of the stack.
             {
-                ans[stk.top()] = i - stk.top();//these many days it will take for "stk.top()" to come to "i", "i" is warmer
-                stk.pop();//since st.top() is now calculated
+                ans[stk.top()] = i - stk.top();//these many days it will take for "stk.top()" to come to "i", "i" is warmer.
+                //for all the indices on the stack while i is warmer, we update their number of days.
+                stk.pop();//since st.top() is now calculated, we pop it so that it does not get overwritten again if a higher temperature comes up again.
             }
             stk.push(i);//if stack is empty or present temperature need to be recorded any way for future. we still have to find warmer day than "i"
         }
