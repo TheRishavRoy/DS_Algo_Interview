87748eef96982e3fc0d826d4b77852e210bd8c16
TheRishavRoy <the.rishav.roy@gmail.com>
2025-05-18 12:26:09 +0530

    (Greedy) LC - 11. Container With Most Water.cpp

---
+++ Array/(Greedy) LC - 11. Container With Most Water.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+	int maxArea(vector<int> &heights)
+	{
+		int n=heights.size();
+		int left=0;
+		int right=n-1;
+		int maxCollectedWater=0;//our result
+		
+		while(left<right)
+		{
+			int localHeight=min(heights[left],heights[right]);//take the bounded height
+			int localWidth=right-left;//cacualte local width
+			maxCollectedWater=max(maxCollectedWater,localHeight*localWidth);
+			
+            //apply greedy method in choosing:
+            if(heights[left]<heights[right])//whichever will be more benificial.
+			{
+				++left;//find better left bar
+			}
+			else
+			{
+				--right;//find better right bar
+			}
+		}
+		return maxCollectedWater;
+	}
+};
