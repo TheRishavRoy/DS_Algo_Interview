68fcf5a58aa526fa379651044f96d21f3883adb4
TheRishavRoy <the.rishav.roy@gmail.com>
2025-07-12 17:53:52 +0530

    (Using HashTable) LC - 2131. Longest Palindrome by Concatenating Two Letter Words.cpp

--- String/(Using HashTable) LC - 2131. Longest Palindrome by Concatenating Two Letter Words.cpp
+++ String/(Using HashTable) LC - 2131. Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -6,29 +6,33 @@ public:
 
         int result = 0;
 
-        for(string &word : words) {
-            string reversedWord = word;
-            swap(reversedWord[0], reversedWord[1]);
+        for(string &word : words) 
+        {
+            string reversedWord = word;//for each Original word, form a reversed word
+            swap(reversedWord[0], reversedWord[1]);//forming reverse word. Since word_len==2(for each word in words) we can easily swap 
 
-            if(mp[reversedWord] > 0) {
-                result += 4;
-                mp[reversedWord]--;
-            } else {
-                mp[word]++;
+            if(mp[reversedWord] > 0)//if freq of reverseWord of word is > 0 
+            {
+                result += 4;//we can increase length of palindrom by 4 since word+reverseWord = 4
+                mp[reversedWord]--;//decrease freq of reverseWord since it is already used
+            } 
+            else 
+            {
+                mp[word]++;//else we push word to be later used as reverseWord
             }
         }
-
-        //Check equal character words. Use only one
-        for(auto &it : mp) {
+        //Check equal character words. Use only one of these words.(in the centre
+        for(auto &it : mp) 
+        {
             string word = it.first;
             int count = it.second;
 
-            if(word[0] == word[1] && count > 0) {
-                result += 2;
+            if(word[0] == word[1] && count > 0) 
+            {
+                result += 2;//using it once(at the centre) increases length by 2
                 break;
             }
         }
-
         return result;
     }
 };
