cdbca64f4eecd8b9b88d3ca62f8138b26f0c579b
TheRishavRoy <the.rishav.roy@gmail.com>
2025-07-15 19:02:10 +0530

    LC - 3307. Find the K-th Character in String Game II.cpp

--- Bit Manipulation/LC - 3307. Find the K-th Character in String Game II.cpp
+++ Bit Manipulation/LC - 3307. Find the K-th Character in String Game II.cpp
@@ -29,3 +29,26 @@ public:
     return 'a' + increases % 26;
   }
 };
+
+/*
+Example:
+Input k = 5 operations = [0,0,0] Output "a" Expected "a"
+============================================================
+
+You determine how many operations are required to cover position k: ceil(log₂(5)) = 3, so we work through 3 steps in reverse.
+
+Step by step:
+-------------
+Step 2: halfSize = 4 → Since k = 5 > 4(k is in right half of doubling), you subtract 4 => (k = k - 4) = 1; So add operations[2] = 0
+Step 1: halfSize = 2 → k = 1 stays in left half; (k = 1 ≤ 2, it’s clearly in the left half, so You leave k unchanged. You don't apply any shift). add 0.
+Step 0: halfSize = 1 → k = 1 still stays in left; add 0
+
+Total shift accumulated: 0
+
+Result: 'a' + (0 % 26) = 'a'
+
+
+Like in step 2 : Because in a string of length 8 (after 3 doublings), the 5th character lies in the right half (positions 5 to 8), which is just a copy of the left half (1 to 4) with a letter shift applied. 
+So we reflect position 5 back to position 1 and track that shift.
+That reflection step is the key insight here—it lets you avoid actually building the full string while still tracking how the character was transformed.
+*/
